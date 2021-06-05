99da5b5b43651cae25442cc427b1c278d7cb13be
MikeStew <mike.a.stewart87@gmail.com>
2021-06-04 23:07:17 -0700

    Added models, user authentication and signup form. Added success.html

--- GamingNewsApp/models.py
+++ GamingNewsApp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import re
+import bcrypt
 
-# Create your models here.
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+class UserManager(models.Manager):
+    def validate(self, form):
+        errors = {}
+        if len(form['username']) < 4:
+            errors['username'] = 'Username must be at least 4 characters'
+        if not EMAIL_REGEX.match(form['email']):
+            errors['email'] = 'Invalid Email Address'
+        username_check = self.filter(username=form['username'])
+        if username_check:
+            errors['username'] = "Username already in use"
+        if len(form['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+        if form['password'] != form['confirm']:
+            errors['password'] = 'Passwords do not match'
+        return errors
+    
+    def authenticate(self, username, password):
+        users = self.filter(username=username)
+        if not users:
+            return False
+
+        user = users[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+    def register(self, form):
+        pw = bcrypt.hashpw(form['password'].encode(), bcrypt.gensalt()).decode()
+        return self.create(
+            username = form['username'],
+            email = form['email'],
+            password = pw,
+        )
+        
+class User(models.Model):
+    username = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    objects = UserManager()
--- GamingNewsApp/urls.py
+++ GamingNewsApp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('signup', views.signup),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('success', views.success)
 ]
